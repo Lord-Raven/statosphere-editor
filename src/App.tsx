1be4587b7e427406b920247b92a2f5199f6df0ea
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-18 19:07:46 -0400

    Experimenting with react-json-schema-form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from '@rjsf/core';
+import { RJSFSchema } from '@rjsf/utils';
+import validator from '@rjsf/validator-ajv8';
+import classifierSchema from "./assets/classifier-schema.json";
+import promptSchema from "./assets/prompt-schema.json";
+import variableSchema from "./assets/variable-schema.json";
+
+const schema: RJSFSchema = {
+  title: 'Statosphere Configuration Editor',
+  type: 'string',
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form schema={schema} validator={validator} />
       </header>
     </div>
   );
