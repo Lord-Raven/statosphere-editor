d9c75459ec798eea2a1f186544b867dc1284cf8c
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-19 11:08:56 -0400

    Experimenting with react-json-schema-form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
-import Form from '@rjsf/material-ui';
+import Form from '@rjsf/antd';
 import validator from '@rjsf/validator-ajv8';
 import classifierSchema from "./assets/classifier-schema.json";
 import promptSchema from "./assets/prompt-schema.json";
 import variableSchema from "./assets/variable-schema.json";
+import variableUiSchema from "./assets/variable-ui-schema.json";
+import ObjectFieldTemplate from './ObjectFieldTemplate';
 
-const schemaClassifier: any = classifierSchema
-const schemaPrompt: any = promptSchema
-const schemaVariable: any = variableSchema
+const schemaClassifier: any = classifierSchema;
+const schemaPrompt: any = promptSchema;
+const schemaVariable: any = variableSchema;
+const uiSchemaVariable: any = variableUiSchema;
 
 function App() {
   return (
@@ -17,7 +20,12 @@ function App() {
         Statosphere Configuration Builder
       </header>
       <h1>Variables</h1>
-      <Form schema={schemaVariable} validator={validator}/>
+      <Form
+        schema={schemaVariable}
+        validator={validator}
+        templates={{ObjectFieldTemplate: ObjectFieldTemplate}}
+        uiSchema={uiSchemaVariable}
+          />
       <h1>Classifiers</h1>
       <Form schema={schemaClassifier} validator={validator}/>
       <h1>Prompt</h1>
