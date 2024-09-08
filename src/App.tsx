ca68f52b5a34ef9fb967b2be445855fb97454289
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-09-07 22:54:24 -0400

    Adding classifier condition and other feature/formatting changes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {ConfigProvider, theme} from 'antd';
+import {Button, ConfigProvider, Input, theme} from 'antd';
 import classifierSchema from "./assets/classifier-schema.json";
 import contentSchema from "./assets/content-schema.json";
 import functionSchema from "./assets/function-schema.json";
@@ -9,9 +9,65 @@ import contentUiSchema from "./assets/content-ui-schema.json";
 import functionUiSchema from "./assets/function-ui-schema.json";
 import variableUiSchema from "./assets/variable-ui-schema.json";
 import {BlurForm} from "./BlurForm";
-
+import React, {useCallback, useEffect, useState} from "react";
+import {CopyToClipboard} from "react-copy-to-clipboard";
+import { CopyOutlined } from '@ant-design/icons';
 
 function App() {
+
+    const [classifierData, setClassifierData] = useState({});
+    const [classifierJson, setClassifierJson] = useState('[]');
+
+    const [contentData, setContentData] = useState({});
+    const [contentJson, setContentJson] = useState('[]');
+
+    const [functionData, setFunctionData] = useState({});
+    const [functionJson, setFunctionJson] = useState('[]');
+
+    const [variableData, setVariableData] = useState({});
+    const [variableJson, setVariableJson] = useState('[]');
+
+    const [fullJson, setFullJson] = useState(`{"classifiers":[],"content":[],"functions":[],"variables":[]}`);
+    const inputStyle = {display: 'flex', paddingLeft: '8px', paddingRight: '8px'};
+    const footerStyle = {
+        position: 'fixed' as 'fixed',
+        bottom: 0,
+        width: '100%',
+        boxShadow: '0 -2px 0px rgba(0, 0, 0, 0.1)',
+        display: 'flex',
+        justifyContent: 'center',
+    };
+
+    const [formCopyStatus, setFormCopyStatus] = useState(false);
+    const onCopyStatus = () => {
+        setFormCopyStatus(true);
+        setTimeout(() => setFormCopyStatus(false), 1500);
+    };
+
+    const handleJsonChange = useCallback((json: string) => {
+        setFullJson(json);
+        let data = JSON.parse(json);
+        setClassifierJson(JSON.stringify(data.classifiers));
+        setContentJson(JSON.stringify(data.content));
+        setFunctionJson(JSON.stringify(data.functions));
+        setVariableJson(JSON.stringify(data.variables));
+    }, [fullJson, classifierData, contentData, functionData, variableData, classifierJson, contentJson, functionJson, variableJson]);
+
+    const handleJsonBlur = useCallback(() => {
+        try {
+            let data = JSON.parse(fullJson);
+            setClassifierData(data.classifiers);
+            setContentData(data.content);
+            setFunctionData(data.functions);
+            setVariableData(data.variables);
+        } catch (error) {
+            console.log(error);
+        }
+    }, [fullJson, classifierData, contentData, functionData, variableData, classifierJson, contentJson, functionJson, variableJson]);
+
+    useEffect(() => {
+        setFullJson(`{"classifiers":${classifierJson ?? '[]'},"content":${contentJson ?? '[]'},"functions":${functionJson ?? '[]'},"variables":${variableJson ?? '[]'}}`);
+    }, [fullJson, classifierJson, contentJson, functionJson, variableJson]);
 
     return (
         <div className="App">
@@ -23,29 +79,62 @@ function App() {
                 <BlurForm
                     schema={variableSchema}
                     uiSchema={variableUiSchema}
+                    formData={variableData}
+                    formJson={variableJson}
+                    setFormData={setVariableData}
+                    setFormJson={setVariableJson}
                 />
 
                 <h1>Functions</h1>
                 <BlurForm
                     schema={functionSchema}
                     uiSchema={functionUiSchema}
+                    formData={functionData}
+                    formJson={functionJson}
+                    setFormData={setFunctionData}
+                    setFormJson={setFunctionJson}
                 />
 
                 <h1>Classifiers</h1>
                 <BlurForm
                     schema={classifierSchema}
                     uiSchema={classifierUiSchema}
+                    formData={classifierData}
+                    formJson={classifierJson}
+                    setFormData={setClassifierData}
+                    setFormJson={setClassifierJson}
                 />
 
-                <h1>Content Modifications</h1>
+                <h1>Content Modifiers</h1>
                 <BlurForm
                     schema={contentSchema}
                     uiSchema={contentUiSchema}
+                    formData={contentData}
+                    formJson={contentJson}
+                    setFormData={setContentData}
+                    setFormJson={setContentJson}
                 />
+                <br/>
+                <div style={footerStyle}>
+                    <header className="App-footer">
+                        <div style={inputStyle}>
+                            <Input value={fullJson}
+                                   onChange={(e) => handleJsonChange(e.target.value)}
+                                   onBlur={() => handleJsonBlur()}
+                                   placeholder="Build structure above or paste JSON here."/>
+                            <CopyToClipboard text={fullJson} onCopy={() => onCopyStatus()}
+                                             className="button" data-clipboard-action="copy">
+                                <Button type={formCopyStatus ? "default" : "primary"} icon={formCopyStatus ? null : <CopyOutlined />} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '5vw' }}>{formCopyStatus ? 'Copied!' : ''}</Button>
+                            </CopyToClipboard>
+                        </div>
+                        <div>
+                            Visit <a href='https://venus.chub.ai/extensions/Ravenok/statosphere-3704059fdd7e'>the
+                            stage</a> for more
+                            information or examples.
+                        </div>
+                    </header>
+                </div>
             </ConfigProvider>
-            <header className="App-footer">
-                Visit <a href='https://venus.chub.ai/extensions/Ravenok/statosphere-3704059fdd7e'>the stage</a> for more information or examples.
-            </header>
         </div>
     );
 }
--- src/BlurForm.tsx
+++ src/BlurForm.tsx
@@ -1,5 +1,5 @@
 import {ArrayFieldTitleProps} from "@rjsf/utils";
-import React, {useCallback, useMemo, useState} from "react";
+import React, {Dispatch, SetStateAction, useCallback, useMemo, useState} from "react";
 import fastJson from "fast-json-stringify";
 import Form from "@rjsf/antd";
 import validator from "@rjsf/validator-ajv8";
@@ -7,8 +7,6 @@ import ObjectFieldTemplate from "./ObjectFieldTemplate";
 import ArrayFieldTemplate from "./ArrayFieldTemplate";
 import ArrayFieldItemTemplate from "./ArrayFieldItemTemplate";
 import TextareaWidget from "./TextareaWidget";
-import {Button, Input} from "antd";
-import {CopyToClipboard} from "react-copy-to-clipboard";
 
 const ArrayFieldTitleTemplate = React.memo((props: ArrayFieldTitleProps) => {
     const {title} = props;
@@ -18,45 +16,29 @@ const ArrayFieldTitleTemplate = React.memo((props: ArrayFieldTitleProps) => {
 interface BlurFormProps {
     schema: any;
     uiSchema: any;
+    formData: any;
+    formJson: string;
+    setFormData:  Dispatch<SetStateAction<{}>>;
+    setFormJson:  Dispatch<SetStateAction<string>>;
 }
 
-export const BlurForm: React.FC<BlurFormProps> = ({schema, uiSchema}) => {
-    const [formData, setFormData] = useState({});
-    const [formJson, setFormJson] = useState('');
-    const [formCopyStatus, setFormCopyStatus] = useState(false);
+export const BlurForm: React.FC<BlurFormProps> = ({schema, uiSchema, formData, formJson, setFormData, setFormJson}) => {
     const formStringify = useMemo(() => fastJson(schema), [schema]);
-    const inputStyle = {display: 'flex', paddingLeft: '8px', paddingRight: '8px'};
-
-    const onCopyStatus = () => {
-        setFormCopyStatus(true);
-        setTimeout(() => setFormCopyStatus(false), 1500);
-    };
 
     const handleDataChange = useCallback((data: any) => {
         setFormData(data);
-    }, []);
-
-    const handleJsonChange = useCallback((data: string) => {
-        setFormJson(data);
-    }, []);
+    }, [formData]);
 
     const handleDataBlur = useCallback(() => {
+        let json = '';
         try {
-            setFormJson(formStringify(formData));
+            json = formStringify(formData);
         } catch (error) {
             console.log(error);
-            setFormJson(JSON.stringify(formData));
+            json = JSON.stringify(formData);
         }
-    }, [formData, formStringify]);
-
-    const handleJsonBlur = useCallback(() => {
-        try {
-            setFormData(JSON.parse(formJson));
-        } catch (error) {
-            console.log(error);
-        }
-    }, [formJson]);
-
+        setFormJson(json);
+    }, [formData, formJson, formStringify]);
     return (
         <div>
             <Form
@@ -77,17 +59,6 @@ export const BlurForm: React.FC<BlurFormProps> = ({schema, uiSchema}) => {
                     TextareaWidget: TextareaWidget
                 }}
             />
-            <div style={inputStyle}>
-                <Input value={formJson}
-                       onChange={(e) => handleJsonChange(e.target.value)}
-                       onBlur={() => handleJsonBlur()}
-                       placeholder="Build structure above or paste JSON here."/>
-                <CopyToClipboard text={formJson} onCopy={() => onCopyStatus()}
-                                 className="button" data-clipboard-action="copy"
-                                 data-clipboard-target="#classifierInput">
-                    <Button danger={formCopyStatus}>{formCopyStatus ? 'Copied!' : 'Copy'}</Button>
-                </CopyToClipboard>
-            </div>
         </div>
     );
 };
